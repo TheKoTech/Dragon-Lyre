cd089d7b15f989e158b0538be1e933c0dabe9fd3
Jegern <severuanov2001@mail.ru>
2022-11-07 12:44:33 +0500

    : 2022-11-07 12:44:33

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The App.js has been edited.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo"/>
+
+				<Button/>
+
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Button = () => {
+	/**
+	 * Creates a directory with given name.
+	 * @param {string} name
+	 */
+	function _createDirectory(name) {
+
+	}
+
+	return (
+		<button style={{width: 200, height: 50, fontSize: 20}}>
+			Play sound
+		</button>
+	);
+};
+
+export default Button;
