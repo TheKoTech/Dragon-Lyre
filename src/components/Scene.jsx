8a319f2ffa98a3195300e41041b08a41f3eb46ac
Dmitry Severyanov <severuanov2001@mail.ru>
2022-11-09 20:53:50 +0500

    Add Web Audio API for play sound.

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -5,37 +5,70 @@ import SoundAddBtn from './SoundAddBtn';
 
 
 function Scene() {
-	const [soundsList, setSoundsList] = useState([{
-		id: 0,
-		title: 'Sound item 1',
-		isPlayed: false,
-		isLooped: true,
-		volume: 50,
-		audioFile: undefined
-	}]);
+	const [soundsList, setSoundsList] = useState([
+		{
+			id: 0,
+			title: 'Sound item 1',
+			isPlayed: false,
+			isLooped: true,
+			volume: 50,
+			audioFile: undefined,
+			soundBuffer: undefined,
+			soundSource: undefined,
+		}
+	]);
+
+	// const audioContext = new AudioContext();
+	// let soundsBuffers = [];
+	// let soundSource;
+	// setupSounds(['/sounds/10000208.mp3']).then((response) => {
+	// 	soundsBuffers = response;
+	// 	console.log('All sounds are ready!');
+	// });
 
 	/**
 	 * @param {number} id номер звука
 	 */
 	const handlePlayBtn = id => {
-		// следи за постоянством кода. Убрал точки с запятыми
-		// Значение не меняется - ставим const
-		const audio = new Audio('/sounds/10000208.ogg');
-		const promise = audio.play();
-
-		if (promise !== undefined) {
-			promise.then(_ => {
-				console.log('Audio start playing.');
-			}).catch(error => {
-				// Ты используешь backticks, обратный слеш для кавычек не нужен
-				console.log(`Audio can't be played. (${error})`);
-			});
+		if (soundSources !== undefined) {
+			for (const soundSource of soundSources) {
+				soundSource.stop();
+			}
+		}
+		for (const soundBuffer of soundsBuffers) {
+			soundSources.push(playSound(soundBuffer));
 		}
 	};
 
+	function playSound(audioBuffer) {
+		const source = audioContext.createBufferSource();
+		source.buffer = audioBuffer;
+		source.connect(audioContext.destination);
+		source.start();
+
+		return source;
+	}
+
+	async function setupSounds(filePaths) {
+		const audioBuffers = [];
+
+		for (const filePath of filePaths) {
+			const audioBuffer = await getAudioBufferFromFile(filePath);
+			audioBuffers.push(audioBuffer);
+		}
+
+		return audioBuffers;
+	}
+
+	async function getAudioBufferFromFile(filePath) {
+		const response = await fetch(filePath);
+		const arrayBuffer = await response.arrayBuffer();
+		return await audioContext.decodeAudioData(arrayBuffer);
+	}
+
 	/**
-	 * @param {Event} e 
-	 * @param {number} id 
+	 * @param {Event} e
+	 * @param {number} id
 	 */
 	const handleVolumeChange = (e, id) => {
 		setSoundsList((prevList) => {
@@ -44,14 +77,14 @@ function Scene() {
 					? { ...sound, volume: e.target.value }
 					: sound;
 			});
-			console.log(`Sound ${id} { volume: ${newList[id].volume} }`);
+			console.log(`Sound ${ id } { volume: ${ newList[id].volume } }`);
 			return newList;
 		});
 	};
 
 	/**
-	 * @param {Event} e 
-	 * @param {number} id 
+	 * @param {Event} e
+	 * @param {number} id
 	 */
 	const handleLoopedChange = (e, id) => {
 		setSoundsList((prevList) => {
@@ -60,7 +93,7 @@ function Scene() {
 					? { ...sound, isLooped: e.target.checked }
 					: sound;
 			});
-			console.log(`Sound ${id} { isLooped: ${newList[id].isLooped} }`);
+			console.log(`Sound ${ id } { isLooped: ${ newList[id].isLooped } }`);
 			return newList;
 		});
 	};
@@ -71,11 +104,7 @@ function Scene() {
 			...soundsList,
 			{
 				id: newId,
-				title: `Sound item ${newId + 1}`,
-				isPlayed: false,
-				isLooped: true,
-				volume: 50,
-				audioFile: undefined
+				title: `Sound item ${ newId + 1 }`,
 			}
 		]);
 	};
@@ -83,7 +112,7 @@ function Scene() {
 	const deleteSound = (id) => {
 		setSoundsList(prevList =>
 			prevList.filter(sound => sound.id !== id)
-		)
+		);
 	};
 
 	return (
@@ -91,20 +120,20 @@ function Scene() {
 			Scene component
 
 			<div className='sounds_list'>
-				{soundsList.map((props) =>
+				{ soundsList.map((props) =>
 					<Sound
-						key={props.id}
-						{...props}
-						onPlayBtn={handlePlayBtn}
-						onVolumeChange={handleVolumeChange}
-						onLoopedChange={handleLoopedChange}
-						onDelete={deleteSound}
+						key={ props.id }
+						{ ...props }
+						onPlayBtn={ handlePlayBtn }
+						onVolumeChange={ handleVolumeChange }
+						onLoopedChange={ handleLoopedChange }
+						onDelete={ deleteSound }
 					/>
-				)}
-				<SoundAddBtn onClick={addSound} />
+				) }
+				<SoundAddBtn onClick={ addSound }/>
 			</div>
 		</div>
-	)
+	);
 }
 
-export default Scene
+export default Scene;
