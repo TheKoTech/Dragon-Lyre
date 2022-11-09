1ca110df5ace17db2c0ed974ecabbaeb077b4401
TheKoTech <thekotech@gmail.com>
2022-11-09 21:40:40 +0500

    moved audioContext to App.js and turned it to state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 import Scene from './components/Scene';
 
 
 function App() {
+	const [audioContext, setAudioContext] = useState(new AudioContext());
 	return (
 		<div className="App">
-			<Scene />
+			<Scene audioContext={audioContext}/>
 		</div>
 	);
 }
