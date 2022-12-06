37b046443050b4b591c2eeabb1e8a07054181058
Jegern <severunov@gmail.com>
2022-12-07 00:27:23 +0500

    The logic in `Scene.jsx` was moved to the separate file. The user can choose multiple sounds via open dialog.

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { SceneSidebar } from './SceneSidebar';
-import Sound from './Sound';
-import SoundAddBtn from './SoundAddBtn';
-import { Toolbar } from './Toolbar';
 import './css/Scene.css';
+
+import Sound from './Sound';
+import Toolbar from './Toolbar';
+import SoundAddBtn from './SoundAddBtn';
+import SceneSidebar from './SceneSidebar';
+import SoundManagement from '../modules/sound-management';
+
 
 /**
  * @param {Object} Scene
@@ -18,18 +21,23 @@ function Scene({ audioContext }) {
 
 	/**
 	 * @typedef {Object} SoundParameters
-	 * @property {number} id
-	 * @property {string} title
-	 * @property {string} extension
-	 * @property {number} volume
-	 * @property {number | undefined} startedAt
-	 * @property {number | undefined} stoppedAt
-	 * @property {boolean} isPlaying
+	 * @property {number} id Sound ID.
+	 * @property {string} title Representing the sound name.
+	 * @property {string} extension File extension.
+	 * @property {number} volume Sound volume (in the range from 0 to 1).
+	 * @property {number | undefined} startedAt Fractional number representing the start time of the sound (by time in
+	 *     the AudioContext).
+	 * @property {number | undefined} stoppedAt Fractional number representing the stop time of the sound (by time in
+	 *     the AudioContext).
+	 * @property {boolean} isPlaying Indicates whether the sound is playing. By default, is false.
 	 * @property {number} minInterval
 	 * @property {number} maxInterval
-	 * @property {AudioBuffer} buffer
-	 * @property {GainNode | undefined} gainNode
-	 * @property {AudioBufferSourceNode | undefined} source
+	 * @property {AudioBuffer} buffer An object that contains all information about the sound file. From it, you can
+	 *     get AudioBufferSourceNode.
+	 * @property {GainNode | undefined} gainNode An object that allowing you to control the volume of the sound.
+	 *     Connected to the AudioContext
+	 * @property {AudioBufferSourceNode | undefined} source An object that represents a thread that directly controls
+	 *     audio playback. Connected to GainNode.
 	 */
 
 
@@ -41,227 +49,32 @@ function Scene({ audioContext }) {
 	/**
 	 * @type [SoundParameters[], Dispatch<SetStateAction<SoundParameters[]>>]
 	 */
-	const soundState = useState([]);
-	const [soundsList, setSoundsList] = soundState;
-	const [sceneTitle, setSceneTitle] = useState('New Scene');
+	const soundListState = useState([]);
+	const [soundsList, setSoundsList] = soundListState;
+	/**
+	 * @type [string, Dispatch<SetStateAction<string>>]
+	 */
+	const sceneTitleState = useState('New Scene');
+	const [sceneTitle, setSceneTitle] = sceneTitleState;
 
 	useEffect(() => {
 		const sceneSave = window['electronAPI'].readSceneSave('./public/saves/scene_name.json');
-		sceneSave.then(resolve => {
-			const json = JSON.parse(resolve);
-			let id = 0;
 
-			setSoundsList(json.map(soundJson => {
+		sceneSave.then(resolve => {
+			let id = SoundManagement.getNewElementId(soundsList);
+
+			SoundManagement.addNewSoundsInSoundList(audioContext, soundListState, JSON.parse(resolve).map(soundJson => {
 				return {
 					id: id += 1,
 					title: soundJson.title,
 					extension: soundJson.extension,
 					volume: soundJson.volume,
-					isPlaying: false,
 					minInterval: soundJson.minInterval,
 					maxInterval: soundJson.maxInterval,
-					buffer: getAudioBufferFromFile(`/sounds/${ soundJson.title + '.' + soundJson.extension }`)
 				};
 			}));
-		}).catch(error => error);
+		}).catch(error => console.error(error));
 	}, [setSoundsList]);
-
-
-	// ========================================
-	// Logic
-	// ========================================
-
-
-	/**
-	 * Starts the sound with the given ID.
-	 * @param {number} id Sound ID.
-	 * @param {number} when
-	 * @param {number} offset
-	 */
-	function startSound(id, when = 0, offset = 0) {
-		const sound = soundsList.find(sound => sound.id === id);
-
-		addToSoundNewSourceAndGain(id);
-
-		if (sound.stoppedAt) {
-			if (sound.stoppedAt - sound.startedAt >= 0) {
-				offset = sound.stoppedAt - sound.startedAt;
-			} else {
-				when = sound.startedAt - sound.stoppedAt;
-			}
-		}
-		sound.source.start(audioContext.currentTime + when, offset);
-		sound.isPlaying = true;
-		sound.startedAt = audioContext.currentTime - offset + when;
-		sound.stoppedAt = undefined;
-	}
-
-	/**
-	 * Stops the sound with the given ID.
-	 * @param {number} id Sound ID.
-	 * @param {Boolean} disconnect If true, disconnects the source from the context.
-	 */
-	function stopSound(id, disconnect = false) {
-		const sound = soundsList.find(sound => sound.id === id);
-
-		if (sound.source) {
-			if (disconnect) {
-				sound.source.disconnect();
-			} else {
-				sound.source.stop();
-			}
-			sound.isPlaying = false;
-			sound.stoppedAt = audioContext.currentTime;
-		}
-	}
-
-	// todo: refactor
-	/**
-	 * Creates SourceNode and GainNode.
-	 * Transfers the settings from the sound with the given ID.
-	 * Attaches SourceNode and GainNode to the sound.
-	 * @param id Sound ID.
-	 */
-	function addToSoundNewSourceAndGain(id) {
-		const sound = soundsList.find(sound => sound.id === id);
-
-		const [sourceNode, gainNode] = createSourceAndGainFromAudioBuffer(sound.buffer);
-		applySettingsFromSoundToSourceAndGain(id, sourceNode, gainNode);
-		attachSourceAndGainToSound(id, sourceNode, gainNode);
-	}
-
-	// todo: refactor
-	/**
-	 * Creates audio source (thread) with gain and connects it to the context.
-	 * @param {AudioBuffer} audioBuffer
-	 * @returns {[AudioBufferSourceNode, GainNode]}
-	 */
-	function createSourceAndGainFromAudioBuffer(audioBuffer) {
-		const sourceNode = audioContext.createBufferSource();
-		const gainNode = audioContext.createGain();
-		sourceNode.buffer = audioBuffer;
-		sourceNode.connect(gainNode);
-		gainNode.connect(audioContext.destination);
-
-		return [sourceNode, gainNode];
-	}
-
-	// todo: refactor
-	/**
-	 * Must run before attaching to the sound.
-	 * @param {number} id Sound ID.
-	 * @param {AudioBufferSourceNode} sourceNode The following sound fields will be transferred: loop and onended.
-	 * @param {GainNode} gainNode The following sound fields will be transferred: value.
-	 */
-	function applySettingsFromSoundToSourceAndGain(id, sourceNode, gainNode) {
-		const sound = soundsList.find(sound => sound.id === id);
-
-		if (sound.source && sound.gainNode) {
-			gainNode.gain.value = sound.volume;
-			sourceNode.onended = sound.source.onended;
-		}
-	}
-
-	// todo: refactor
-	/**
-	 * Must run after applying the settings to the source and the gain.
-	 * @param {number} id Sound ID.
-	 * @param {AudioBufferSourceNode} sourceNode
-	 * @param {GainNode} gainNode
-	 */
-	function attachSourceAndGainToSound(id, sourceNode, gainNode) {
-		const sound = soundsList.find(sound => sound.id === id);
-
-		sound.source = sourceNode;
-		sound.gainNode = gainNode;
-		sound.source.onended = () => {
-			handleSoundEnd(sound.id, sound.maxInterval);
-		};
-	}
-
-	// todo: refactor
-	/**
-	 * Initializes AudioBuffer to the sound. Creates an object Sound and add it to the Scene.
-	 * @param {File} file
-	 */
-	function setupSoundAndAddInSoundList(file) {
-		const filePath = '/sounds/' + file.name;
-		getAudioBufferFromFile(filePath).then(audioBuffer => {
-			addSoundInSoundList(file.name, audioBuffer);
-		});
-	}
-
-	/**
-	 * Decodes audio data from the file and creates audio buffer.
-	 * @param {string} filePath The path must contain the file extension.
-	 * @returns {Promise<AudioBuffer>}
-	 */
-	async function getAudioBufferFromFile(filePath) {
-		const response = await fetch(filePath);
-		const arrayBuffer = await response.arrayBuffer();
-		return await audioContext.decodeAudioData(arrayBuffer);
-	}
-
-	// todo: remove this absolutely disgusting dumpster of a function
-	/**
-	 * Opens a system dialog.
-	 * @param {string} contentType The files you wish to select. For instance, use "image/*" to select all types of
-	 *     images.
-	 * @param {Boolean} multiple Indicates if the user can select multiple files.
-	 * @returns {Promise<File|File[]>} A promise of a file or array of files if the multiple parameter is true.
-	 */
-	function selectFile(contentType, multiple) {
-		return new Promise(resolve => {
-			const input = document.createElement('input');
-			input.type = 'file';
-			input.multiple = multiple;
-			input.accept = contentType;
-
-			input.onchange = () => {
-				const files = Array.from(input.files);
-				if (multiple)
-					resolve(files);
-				else
-					resolve(files[0]);
-			};
-
-			input.click();
-		});
-	}
-
-	/**
-	 * Starts a sound and then creates an object Sound and add it to the Scene.
-	 * @param {string} fileName
-	 * @param {AudioBuffer} audioBuffer A buffer containing audio information.
-	 */
-	function addSoundInSoundList(fileName, audioBuffer) {
-		setSoundsList([
-			...soundsList,
-			{
-				id: getNewSoundId(),
-				title: fileName.substring(0, fileName.lastIndexOf('.')),
-				extension: fileName.substring(fileName.lastIndexOf('.') + 1),
-				volume: 1.0,
-				isPlaying: false,
-				minInterval: 0,
-				maxInterval: 5,
-				buffer: audioBuffer,
-			}
-		]);
-	}
-
-	/**
-	 * Returns an ID for a new sound.
-	 * @returns {number}
-	 */
-	function getNewSoundId() {
-		let id;
-		if (soundsList.length === 0)
-			id = 0;
-		else
-			id = Math.max(...soundsList.map(sound => sound.id)) + 1;
-		return id;
-	}
 
 
 	// ========================================
@@ -270,7 +83,6 @@ function Scene({ audioContext }) {
 
 
 	/**
-	 *
 	 * @param {ChangeEvent<HTMLInputElement>} e
 	 * @param {number} id Sound ID.
 	 */
@@ -285,20 +97,18 @@ function Scene({ audioContext }) {
 	};
 
 	/**
-	 *
 	 * @param {number} id Sound ID.
 	 */
 	const handlePlayBtn = (id) => {
 		const sound = soundsList.find(sound => sound.id === id);
 
 		if (sound.isPlaying)
-			stopSound(id);
+			SoundManagement.stopSound(sound, audioContext.currentTime);
 		else
-			startSound(id);
+			SoundManagement.startSound(audioContext, sound);
 	};
 
 	/**
-	 *
 	 * @param {ChangeEvent<HTMLInputElement>} e
 	 * @param {number} id Sound ID.
 	 */
@@ -317,7 +127,6 @@ function Scene({ audioContext }) {
 	};
 
 	/**
-	 *
 	 * @param {ChangeEvent<HTMLInputElement>} e
 	 * @param {number} id Sound ID.
 	 */
@@ -332,10 +141,22 @@ function Scene({ audioContext }) {
 	};
 
 	/**
-	 *
 	 */
 	const handleAddSound = () => {
-		selectFile('', false).then(file => setupSoundAndAddInSoundList(file));
+		SoundManagement.selectFiles().then(files => {
+			let id = SoundManagement.getNewElementId(soundsList);
+
+			SoundManagement.addNewSoundsInSoundList(audioContext, soundListState, files.map(file => {
+				return {
+					id: id += 1,
+					title: file.name.substring(0, file.name.lastIndexOf('.')),
+					extension: file.name.substring(file.name.lastIndexOf('.') + 1),
+					volume: 1.0,
+					minInterval: 0,
+					maxInterval: 5,
+				}
+			}))
+		});
 	};
 
 	/**
@@ -343,27 +164,14 @@ function Scene({ audioContext }) {
 	 * @param {number} id Sound ID.
 	 */
 	const handleDeleteBtn = (id) => {
-		stopSound(id, true);
+		const sound = soundsList.find(sound => sound.id === id);
+		SoundManagement.stopSound(audioContext, sound, true);
+
 		setSoundsList(prevList => prevList.filter(sound => sound.id !== id));
 	};
 
 	/**
-	 * Start the sound with the given ID after a certain amount of time (default 0).
-	 * Before start, add to the sound new source and new gain.
-	 * @param {number} id Sound ID.
-	 * @param {number} when The time, in seconds (attached to AudioContext), at which the sound should start playing.
-	 */
-	function handleSoundEnd(id, when = 0) {
-		const sound = soundsList.find(sound => sound.id === id);
-
-		if (sound.isPlaying) {
-			startSound(id, when);
-		}
-	}
-
-	/**
-	 *
-	 * @param {Event} e
+	 * @param {MouseEvent<HTMLButtonElement>} e
 	 */
 	function handleOnSave(e) {
 		const jsonString = JSON.stringify(soundsList.map(sound => {
---
+++ src/modules/sound-management.jsx
@@ -0,0 +1,211 @@
+export default class SoundManagement {
+
+	// ========================================
+	// Types
+	// ========================================
+
+
+	/**
+	 * @typedef {Object} SoundParameters
+	 * @property {number} id Sound ID.
+	 * @property {string} title Representing the sound name.
+	 * @property {string} extension File extension.
+	 * @property {number} volume Sound volume (in the range from 0 to 1).
+	 * @property {number | undefined} startedAt Fractional number representing the start time of the sound (by time in
+	 *     the AudioContext).
+	 * @property {number | undefined} stoppedAt Fractional number representing the stop time of the sound (by time in
+	 *     the AudioContext).
+	 * @property {boolean} isPlaying Indicates whether the sound is playing. By default, is false.
+	 * @property {number} minInterval
+	 * @property {number} maxInterval
+	 * @property {AudioBuffer} buffer An object that contains all information about the sound file. From it, you can
+	 *     get AudioBufferSourceNode.
+	 * @property {GainNode | undefined} gainNode An object that allowing you to control the volume of the sound.
+	 *     Connected to the AudioContext
+	 * @property {AudioBufferSourceNode | undefined} source An object that represents a thread that directly controls
+	 *     audio playback. Connected to GainNode.
+	 */
+
+
+	// ========================================
+	// Adding Sound
+	// ========================================
+
+	/**
+	 * @returns {Promise<File[]>} A promise of a file or array of files if the multiple parameter is true.
+	 */
+	static selectFiles() {
+		return new Promise(resolve => {
+			const input = document.createElement('input');
+			input.type = 'file';
+			input.multiple = true;
+
+			input.onchange = () => {
+				resolve(Array.from(input.files));
+			};
+
+			input.click();
+		});
+	}
+
+	/**
+	 * Don't run it function in a row, it works asynchronously.
+	 * @param {AudioContext} audioContext
+	 * @param {[SoundParameters[], Dispatch<SetStateAction<SoundParameters[]>>]} soundListState
+	 * @param {SoundParameters} sound
+	 */
+	static setupSoundAndAddInSoundList(audioContext, soundListState, sound) {
+		const fileName = sound.title + '.' + sound.extension;
+
+		this.getAudioBufferFromFile(audioContext, fileName).then(audioBuffer => {
+			soundListState[1]([
+				...soundListState[0],
+				{
+					...sound,
+					isPlaying: false,
+					buffer: audioBuffer,
+				}
+			]);
+		});
+	}
+
+	/**
+	 * @param {AudioContext} audioContext
+	 * @param {[SoundParameters[], Dispatch<SetStateAction<SoundParameters[]>>]} soundListState
+	 * @param {SoundParameters[]} newSounds
+	 */
+	static addNewSoundsInSoundList(audioContext, soundListState, newSounds) {
+		soundListState[1]([
+			...soundListState[0],
+			...(newSounds.map(sound => {
+				const fileName = sound.title + '.' + sound.extension;
+
+				return {
+					...sound,
+					isPlaying: false,
+					buffer: this.getAudioBufferFromFile(audioContext, fileName),
+				};
+			}))
+		]);
+	}
+
+	/**
+	 * Decodes audio data from the file and creates audio buffer.
+	 * @param {string} fileName The file name must contain the file extension.
+	 * @param {AudioContext} audioContext
+	 * @returns {Promise<AudioBuffer>}
+	 */
+	static async getAudioBufferFromFile(audioContext, fileName) {
+		// todo: Make 'fetch' work with the local resources
+		const response = await fetch('sounds/' + fileName);
+		const arrayBuffer = await response.arrayBuffer();
+		return await audioContext.decodeAudioData(arrayBuffer);
+	}
+
+	/**
+	 * Returns an ID for a new element in the list.
+	 * @returns {number} Integer number.
+	 */
+	static getNewElementId(list) {
+		let id;
+		if (list.length === 0)
+			id = 0;
+		else
+			id = Math.max(...list.map(element => element.id)) + 1;
+		return id;
+	}
+
+
+	// ========================================
+	// Starting Sound
+	// ========================================
+
+
+	/**
+	 * @param {AudioContext} audioContext
+	 * @param {SoundParameters} sound
+	 * @param {number} when
+	 */
+	static startSound(audioContext, sound, when = 0) {
+		this.addToSoundNewSourceAndGain(audioContext, sound);
+
+		let offset = 0;
+		if (sound.stoppedAt) {
+			if (sound.stoppedAt - sound.startedAt >= 0) {
+				offset = sound.stoppedAt - sound.startedAt;
+			} else {
+				when = sound.startedAt - sound.stoppedAt;
+			}
+		}
+		sound.source.start(audioContext.currentTime + when, offset);
+		sound.isPlaying = true;
+		sound.startedAt = audioContext.currentTime - offset + when;
+		sound.stoppedAt = undefined;
+	}
+
+	/**
+	 * Creates SourceNode and GainNode.
+	 * Transfers the settings from the sound with the given ID.
+	 * Attaches SourceNode and GainNode to the sound.
+	 * @param {AudioContext} audioContext
+	 * @param {SoundParameters} sound
+	 */
+	static addToSoundNewSourceAndGain(audioContext, sound) {
+		const [sourceNode, gainNode] = this.createSourceAndGain(audioContext, sound.buffer);
+
+		if (sound.source && sound.gainNode) {
+			gainNode.gain.value = sound.volume;
+			sourceNode.onended = sound.source.onended;
+		} else {
+			// Default values
+			gainNode.gain.value = 0.5;
+			sourceNode.onended = () => {
+				if (sound.isPlaying) {
+					this.startSound(sound, audioContext.currentTime, sound.maxInterval);
+				}
+			};
+		}
+
+		sound.source = sourceNode;
+		sound.gainNode = gainNode;
+	}
+
+	/**
+	 * Creates audio source (thread) with gain and connects it to the context.
+	 * @param {AudioContext} audioContext
+	 * @param {AudioBuffer} audioBuffer
+	 * @returns {[AudioBufferSourceNode, GainNode]}
+	 */
+	static createSourceAndGain(audioContext, audioBuffer) {
+		const sourceNode = audioContext.createBufferSource();
+		const gainNode = audioContext.createGain();
+		sourceNode.buffer = audioBuffer;
+		sourceNode.connect(gainNode);
+		gainNode.connect(audioContext.destination);
+
+		return [sourceNode, gainNode];
+	}
+
+
+	// ========================================
+	// Stopping Sound
+	// ========================================
+
+
+	/**
+	 * @param {AudioContext} audioContext
+	 * @param {SoundParameters} sound
+	 * @param {Boolean} disconnect If true, disconnects the source from the context.
+	 */
+	static stopSound(audioContext, sound, disconnect = false) {
+		if (sound.source) {
+			if (disconnect) {
+				sound.source.disconnect();
+			} else {
+				sound.source.stop();
+			}
+			sound.isPlaying = false;
+			sound.stoppedAt = audioContext.currentTime;
+		}
+	}
+}
