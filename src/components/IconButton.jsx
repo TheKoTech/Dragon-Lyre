7616d6b41c389a9c8a5cee6174c8155c2979c300
Welnyr <thekotech@gmail.com>
2022-12-10 15:36:22 +0500

    Added .active functionality to IconButton

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import * as Icons from '../assets/icons';
 import './css/IconButton.css';
 
-function IconButton({ iconName, label, onClick }) {
+function IconButton({ iconName, label, active, onClick }) {
 	return (
 		<button
 			onClick={ (e) => onClick(e) }
-			className='icon-button'
+			className={ `icon-button${ active ? ` active` : `` }` }
 		>
 			{ Icons[iconName] }
 			{ label ? <label>label</label> : '' }
