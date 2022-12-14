9e5291a9e8a6c621e7ef0271d9d759d1257a3c7b
Jegern <severunov@gmail.com>
2022-12-14 22:37:52 +0500

    Renamed `fullscreen.svg` -> `maximize.svg`. Added window control functions.

--- src/components/WindowControls/WindowControls.jsx
+++ src/components/WindowControls/WindowControls.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import WindowControlsButton from './WindowControlsButton'
-import './WindowControls.css'
+import React from 'react';
+import WindowControlsButton from './WindowControlsButton';
+import './WindowControls.css';
 
 export default function WindowControls() {
+
+	const handleMinimizeBtn = () => {
+		window['electronAPI'].minimize();
+	}
+
+	const handleMaximizeBtn = () => {
+		window['electronAPI'].maximize();
+	}
+
+	const handleCloseBtn = () => {
+		window['electronAPI'].close();
+	}
+
 	return (
 		<div className='window-controls'>
-			<WindowControlsButton iconName='Minimize' />
-			<WindowControlsButton iconName='Fullscreen' />
-			<WindowControlsButton iconName='Close' />
+			<WindowControlsButton iconName='Minimize'
+			                      onClick={ handleMinimizeBtn }/>
+			<WindowControlsButton iconName='Maximize'
+			                      onClick={ handleMaximizeBtn }/>
+			<WindowControlsButton iconName='Close'
+			                      onClick={ handleCloseBtn }/>
 		</div>
-	)
+	);
 }
